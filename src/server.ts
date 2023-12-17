7747edbb62578ca2986577d37385c4198c145c73
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-17 02:45:29 +0200

    update

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+
+var app = express();
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Be3o!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
