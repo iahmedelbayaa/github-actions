e1bafde65100511231e784c72271fa3cd849331d
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-21 20:00:25 +0200

    update-workflow

--- install-runner.sh
+++ install-runner.sh
@@ -1,7 +1,7 @@
 docker run -d --name runner-1 \
     -e GITHUB_USERNAME=iahmedelbayaa \
     -e GITHUB_REPOSITORY=github-actions \
-    -e GITHUB_PERSONAL_ACCESS_TOKEN=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 \
+    -e GITHUB_PERSONAL_ACCESS_TOKEN=\
     -e RUNNER_NAME=githubAction-Runner-1 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /usr/bin/docker:/usr/bin/docker \
